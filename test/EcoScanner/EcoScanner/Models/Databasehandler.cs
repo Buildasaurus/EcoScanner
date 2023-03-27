d4c571c2d8ece881b1849d7783f9fbd12de34244
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-03-27 13:38:10 +0200

    added databasecode

---
+++ test/EcoScanner/EcoScanner/Models/Databasehandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace EcoScanner.Models
+{
+    public class Databasehandler
+    {
+		IFirebaseConfig ifc = new FirebaseConfig()
+		{
+			AuthSecret = "TlXCyZsxp3gJw2IXUP3266N6xvk95GWsUMXZLzdh",
+			BasePath = "https://foedevareklimabelastning-default-rtdb.europe-west1.firebasedatabase.app/"
+		};
+
+		IFirebaseClient client;
+
+		public Databasehandler() { }
+		Product GetProduct(int number)
+		{
+			var result = client.Get("Madvare/" + number);
+			Product product = result.ResultAs<Product>();
+			return product;
+		}
+	}
+}
