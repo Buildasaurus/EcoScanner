a9528e5dba51e756a6c8998bb092eae592efee44
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-04-12 16:04:28 +0200

    copied Jonatans code

--- test/EcoScanner/EcoScanner/ViewModels/SearchViewModel.cs
+++ test/EcoScanner/EcoScanner/ViewModels/SearchViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EcoScanner.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using Xamarin.Forms;
 
@@ -12,10 +14,15 @@ namespace EcoScanner.ViewModels
 			SearchInitilized = new Command(search);
 		}
 		public Command SearchInitilized { get; set; }
-
+		public string Text { get; set; }
 		void search()
 		{
-
+			Databasehandler.LoadAllProducts();
+			List<Product> products = Databasehandler.Search(Text);
+			foreach (Product product in products)
+			{
+				Console.WriteLine(product.Name);
+			}
 		}
 	}
 }
