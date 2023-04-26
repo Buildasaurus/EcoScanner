b3513aa461b8fd31501b994384b3c6d0f06c5b25
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-04-26 11:01:23 +0200

    Dishes database + start dish view

--- test/EcoScanner/EcoScanner/Views/DishesView.xaml.cs
+++ test/EcoScanner/EcoScanner/Views/DishesView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EcoScanner.Models;
+using EcoScanner.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,34 @@ namespace EcoScanner.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DishesView : ContentPage
 	{
+		DishesViewModel dishesViewModel;
 		public DishesView()
 		{
 			InitializeComponent();
+			dishesViewModel = new DishesViewModel();
+			BindingContext = dishesViewModel;
+			setupContent();
+		}
+		async void setupContent()
+		{
+			await Databasehandler.loadDishes();
+			foreach (Dish dish in Databasehandler.dishes.Values)
+			{
+				Frame frame = new Frame();
+				frame.CornerRadius = 10;
+				frame.Content = new DishView(dish);
+				frame.Margin = 10;
+
+
+				var tapGestureRecognizer = new TapGestureRecognizer();
+				tapGestureRecognizer.SetBinding(TapGestureRecognizer.CommandProperty, "TapCommand");
+				tapGestureRecognizer.CommandParameter = dish;
+				frame.GestureRecognizers.Add(tapGestureRecognizer);
+
+
+				frame.BackgroundColor = Color.LightBlue;
+				stack.Children.Add(frame);
+			}
 		}
 	}
 }
