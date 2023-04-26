c1766290defc51666b34831f76bb9a0c4f8bacd1
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-04-26 21:37:53 +0200

    warningpopup + popup design

--- test/EcoScanner/EcoScanner/Views/MyPopup.xaml.cs
+++ test/EcoScanner/EcoScanner/Views/MyPopup.xaml.cs
@@ -24,6 +24,7 @@ namespace EcoScanner.Views
 			BindingContext = _viewModel = new MyPopupViewModel(product, onListe);
 			
 			this.product = product;
+
 		}
 		private void backgroundClosed(object sender, EventArgs e)
 		{
