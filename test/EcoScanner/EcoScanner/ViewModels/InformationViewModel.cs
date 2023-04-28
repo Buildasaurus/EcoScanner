6e81f969eaf321d9790a23aeff4c28fdcb6a9d98
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-04-28 14:13:01 +0200

    Information Page

---
+++ test/EcoScanner/EcoScanner/ViewModels/InformationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoScanner.ViewModels
+{
+    public class InformationViewModel : BaseViewModel
+    {
+        public string Title { get; set; }
+        public InformationViewModel() 
+        {
+            Title = "Information";
+        }
+    }
+}
---
+++ test/EcoScanner/EcoScanner/Views/InformationView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using EcoScanner.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EcoScanner.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InformationView : ContentPage
+	{
+		InformationViewModel viewModel;
+		public InformationView ()
+		{
+			viewModel = new InformationViewModel ();
+			BindingContext = viewModel;
+			InitializeComponent ();
+		}
+	}
+}
